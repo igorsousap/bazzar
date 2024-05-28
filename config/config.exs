c0b72c772737ed91a161606b7d8fc6452d5c1b22
IgorSousa <igorsousapinto140@gmail.com>
2024-05-28 19:48:04 -0300

    adds_module_client_web

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,20 @@
 # move said applications out of the umbrella.
 import Config
 
+config :client_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :client_web, ClientWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [json: ClientWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: ClientWeb.PubSub,
+  live_view: [signing_salt: "KGdPnPm1"]
+
 # Configure Mix tasks and generators
 config :persistence,
   ecto_repos: [Persistence.Repo]
@@ -20,7 +34,6 @@ config :persistence,
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
-import Config
 
 # Configures Elixir's Logger
 config :logger, :console,
