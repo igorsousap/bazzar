c3ec07a2d45f23db1ede6fc65d6009e18b51c78f
IgorSousa <igorsousapinto140@gmail.com>
2024-05-27 23:17:52 -0300

    adds migrations for store and products

---
+++ apps/persistence/priv/repo/migrations/20240528013859_add_product_table.exs
@@ -0,0 +1,34 @@
+defmodule Persistence.Repo.Migrations.AddProductTable do
+  use Ecto.Migration
+
+  def change do
+    create_size_types_enum()
+
+    create table(:products, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :product_name, :string, required: true
+
+      add :id_store,
+          references(:stores, on_delete: :delete_all, column: :id, type: :uuid),
+          null: false
+
+      add :description, :text, required: true
+      add :size, :string, required: true
+      add :value, :integer, required: true
+      add :quantity, :integer, required: true
+      add :picture, :string, required: true
+
+      timestamps()
+    end
+
+    create unique_index(:products, [:product_name], name: :product_name_index)
+  end
+
+  defp create_size_types_enum do
+    query_create_type = "CREATE TYPE size AS ENUM ('xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl')"
+
+    query_create_type_rollback = "DROP TYPE size"
+
+    execute query_create_type, query_create_type_rollback
+  end
+end
